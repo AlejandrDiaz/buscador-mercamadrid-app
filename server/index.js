239cd3638a861a20892e08a19a3a739a0344680d
AlejandrDiaz <alejandrdiaz@gmail.com>
2022-09-08 01:53:36 +0100

    añadiendo fichero servidor express nodejs

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const {Client} = require('@elastic/elasticsearch');
+
+const app = express();
+var client = new Client({
+  node: 'http://localhost:9200',
+  auth: {
+    username: 'elastic',
+    password: '9T_=T9LXugO+Xfjt0UJm'
+  }
+});
+
+
+const PrespuestaElastic  = (eresponde) => {
+  const respuestaH = eresponde.hits.hits;
+  const resultado = respuestaH.map((hit) => hit._source);
+  return resultado;
+};
+        app.use(cors());
+        app.get("/elastic", async (req, res, next) => {
+        try {
+          const { indice = "" } = req.query;
+          const { campo = "" } = req.query;
+          const { text = "" } = req.query;
+          const response = await client.search(
+            {
+              index: indice,
+              from: 0,
+              size:100,
+              body: {
+                query: {
+                  query_string: {
+                    default_field: campo,
+                    query:text,
+                  },
+                },
+              },
+            },
+            {
+              ignore: [404],
+              maxRetries: 3,
+            }
+          );
+          res.json({
+            message: "Búsqueda Satisfactoria",
+            records: PrespuestaElastic(response),
+                      
+            });
+        } catch (err) {
+          next(err);
+        }
+      });
+
+      app.listen(3030, () => {
+        console.log("Servidor escuchando en el puerto 3030");
+      });
+
+
+  
